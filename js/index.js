c10f59c7d0557940f73b544a8803c99f8a4deaf7
mponanwess <118251780+mponanwess@users.noreply.github.com>
2024-01-13 09:04:29 +0100

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const counts = document.querySelectorAll('.count');
+const speed = 119;
+
+counts.forEach((counter) => {
+    function upData(){
+        const target = Number(counter.getAttribute('data-target'));
+        const count = Number(counter.innerHTML);
+        const inc = target / speed;
+
+        if(count < target){
+            counter.innerHTML = Math.floor(inc + count);
+            setTimeout(upData, 1);
+        }
+        else{
+            counter.innerHTML = target;
+        }
+    }
+    upData()
+})
