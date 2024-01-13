841bb9369bd2cd44e73ddec5d11aa5f161cc0cfe
mponanwess <118251780+mponanwess@users.noreply.github.com>
2024-01-13 09:10:52 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,1 @@
-const counts = document.querySelectorAll('.count');
-const speed = 119;
 
-counts.forEach((counter) => {
-    function upData(){
-        const target = Number(counter.getAttribute('data-target'));
-        const count = Number(counter.innerHTML);
-        const inc = target / speed;
-
-        if(count < target){
-            counter.innerHTML = Math.floor(inc + count);
-            setTimeout(upData, 100);
-        }
-        else{
-            counter.innerHTML = target;
-        }
-    }
-    upData()
-})
